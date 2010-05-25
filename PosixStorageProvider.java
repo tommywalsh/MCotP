4d94fc8e7eeff7645a9afceb3ffda0f5055409e9
Tom Walsh <ymmothslaw@gmail.com>
2010-05-25 09:51:34 -0400

    Add filter to only play known music file types

--- PosixStorageProvider.java
+++ PosixStorageProvider.java
@@ -36,11 +36,34 @@ public class PosixStorageProvider implements StorageProvider
 	return getSongPath(song.bandName(), song.albumName(), song.songName());
     }
 
+    static private boolean isPlayableFile(File file) {
+        boolean okay = false;
+        if (file.isFile()) {
+            String filename = file.getName();
+            int dotpos = filename.lastIndexOf('.');
+            if (dotpos != -1) {
+                String ext = filename.substring(dotpos);
+                if (ext.equalsIgnoreCase(".mp3") ||
+                    ext.equalsIgnoreCase(".m4a") ||
+                    ext.equalsIgnoreCase(".ogg") ||
+                    ext.equalsIgnoreCase(".wma") ||
+                    ext.equalsIgnoreCase(".mp2") ||
+                    ext.equalsIgnoreCase(".mpc") ||
+                    ext.equalsIgnoreCase(".wav")) {
+                    okay = true;
+                }
+            }
+        }
+        return okay;
+    }
+
+
     public Vector<String> getFilesOrDirs(String path, boolean getFiles)
     {
 	Vector<String> matches = new Vector<String>();
 	File dir = new File(path);
 	String[] entries = dir.list();
+
 	if (entries.length != 0) {
 	    Arrays.sort(entries);
 	    
@@ -48,8 +71,8 @@ public class PosixStorageProvider implements StorageProvider
 	    for (String entry : entries) {
 		File file = new File(path + "/" + entry);
 		if (getFiles) {
-		    if (file.isFile()) {
-			matches.add(file.getName());
+		    if (isPlayableFile(file)) {
+                        matches.add(file.getName());
 		    }
 		} else if (file.isDirectory()) {
 		    matches.add(file.getName());
