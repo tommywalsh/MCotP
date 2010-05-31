f6a98cd5ced723a733acfdc947751a4d03f956fa
Tom Walsh <ymmothslaw@gmail.com>
2010-05-31 14:45:46 -0400

    Add skeleton android player and beef up build. AndroidPlayer has been added to interface between Engine and Android's MediaPlayer This code is untested as of this checkin. Also, move all useful files out of android-build, which should now be 100% managed by ant

---
+++ android/src/AndroidPlayer.java
@@ -0,0 +1,59 @@
+package mcotp;
+
+import android.media.MediaPlayer;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class AndroidPlayer implements Player
+{
+    StorageProvider m_storage;
+
+    private HashSet<Player.SongFinishedListener> m_listeners = new HashSet<Player.SongFinishedListener>();
+
+    AndroidPlayer(StorageProvider sp) {
+	m_storage = sp;
+	m_player = new MediaPlayer();
+	m_player.setOnCompletionListener(new CListener());
+    }
+
+
+    public void addListener(Player.SongFinishedListener l) {
+	m_listeners.add(l);
+    }
+
+    public void removeListener(Player.SongFinishedListener l) {
+	m_listeners.remove(l);
+    }
+
+
+    private MediaPlayer m_player;
+
+    public void play() {
+	m_player.start();
+    }
+
+    public void pause() {
+	m_player.pause();
+    }
+
+    public void restartSong() {
+	m_player.start();
+    }
+
+    public void setSong(Song song) {
+	m_player.reset();
+	try {
+	    m_player.setDataSource(m_storage.getSongPath(song));
+	    m_player.prepare();
+	} catch (java.io.IOException e) {
+	}
+    }
+
+    private class CListener implements MediaPlayer.OnCompletionListener {
+	public void onCompletion(MediaPlayer p) {
+	    for (Iterator<Player.SongFinishedListener> iter = m_listeners.iterator(); iter.hasNext();  ) {
+		iter.next().onSongFinished();
+	    }	
+	}
+    }
+}
