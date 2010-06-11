3179e23d01d217bfde9095815c5f853bb78501f9
Tom Walsh <ymmothslaw@gmail.com>
2010-06-11 10:00:03 -0400

    Get simple Android GUI up and running Only plays music in random mode (and doesn't turn off!)

--- android/src/com/github/tommywalsh/mcotp/android/AndroidUI.java
+++ android/src/com/github/tommywalsh/mcotp/android/AndroidUI.java
@@ -10,17 +10,10 @@ import android.widget.TextView;
 
 public class AndroidUI extends Activity
 {
-    private void init() {
-	PosixStorageProvider psp = new PosixStorageProvider("/sdcard/music");
-	SongProvider sp = new SongProvider(psp);
-	sp.constructLibrary();
-	AndroidPlayer pl = new AndroidPlayer(psp);
-	final Engine engine = new Engine(sp, pl);
-
-    }
-
-
+    private AndroidPlayer m_player;
+    private SongProvider m_songProvider;
     private TextView m_songText;
+    private Engine m_engine;
 
     /** Called when the activity is first created. */
     @Override
@@ -28,13 +21,43 @@ public class AndroidUI extends Activity
     {
         super.onCreate(savedInstanceState);
 	setContentView(R.layout.main);
+	snagUIElements();
 
-	init();
+	initializeInternals();
+
+    }
+
+    private void snagUIElements() {
 	m_songText = (TextView) findViewById(R.id.songText);
-
+    }
+    
+    private void initializeInternals() {
 	PosixStorageProvider psp = new PosixStorageProvider("/sdcard/music");
-	Vector<String> vs = psp.getFilesOrDirs(psp.getLibraryPath(), false);
-	Integer s = vs.size();
-	m_songText.setText("Number of bands: " + s.toString());
+	m_songProvider = new SongProvider(psp);
+	m_songProvider.constructLibrary();
+
+	m_player = new AndroidPlayer(psp);
+	m_engine = new Engine(m_songProvider, m_player);
+
+	m_engine.addListener(new Engine.UpdateListener() {
+		public void onSongChanged(Song newSong) {
+		    updateSongDisplay(newSong);
+		}
+	    });
+    }
+
+    // should instead restore state from previous session!
+    private void startPlaying() {
+	// should be controlled by UI
+	m_engine.toggleRandom();
+	m_engine.nextSong();
+	
+	m_engine.togglePlayPause();
+
+	updateSongDisplay(m_engine.getSong());
+    }
+
+    private void updateSongDisplay(Song song) {
+	m_songText.setText(song.songName());
     }
 }
