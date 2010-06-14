162703daa99df19846291e4c141412e21a6ce60d
Tom Walsh <ymmothslaw@gmail.com>
2010-06-14 16:02:22 -0400

    Break out Band and Album to separate classes in preparation for serialization. Later, we need to write/read these to/from disk at startup and scan time.

---
+++ src/com/github/tommywalsh/mcotp/shared/Album.java
@@ -0,0 +1,33 @@
+package com.github.tommywalsh.mcotp.shared;
+
+import java.lang.String;
+import java.io.Serializable;
+
+public class Album implements Serializable {
+    private String m_name;
+    private int m_firstSong;
+    private int m_numSongs;
+
+    public int firstSong() {
+	return m_firstSong;
+    }
+
+    public int numSongs() {
+	return m_numSongs;
+    }
+
+    public int lastSong() {
+	return m_firstSong + m_numSongs - 1;
+    }
+
+    public String name() {
+	return m_name;
+    }
+
+    public Album(String name, int firstSong, int numSongs) {
+	m_name = name;
+	m_firstSong = firstSong;
+	m_numSongs = numSongs;
+    }
+}
+
